396d41967b331518c60e06ea9c7b4c9928784d83
Alexis <80banm231106@gmail.com>
2026-03-26 07:55:04 -0600

    prebaff

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ print("--HOLA TODOS--")
 
 print("EL RESULTADO DE 7*4 ES:",resultado)
 
-def multiplicacion2(a,b):
+def multiplicacion2(a,b): 
     return a*b
 
-resultado2 = multiplicacion2(9,9)
+resultado2 = multiplicacion2(9,9) # nuevapruebaff
 
 print("EL SEGUNDO RESULTADO DE 9*9 ES: ", resultado2)
